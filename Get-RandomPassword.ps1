dc9d8d8bc440fffcef75269d65fb9c981d53cd05
Bronkowo <thomas.tro@gmx.de>
2018-01-03 08:40:50 +0100

    first commit

---
+++ Get-RandomPassword.ps1
@@ -0,0 +1,70 @@
+﻿function Get-RandomPassword
+    {
+    Param 
+        (
+        [int]$Number = 1,
+        [int]$Length = 8,
+        [Switch]$AsSecureString
+        )
+    #region Variable
+    $Result       = $null
+    $SecureResult = $null
+    $PlainResult  = $null
+
+    [String]$SmallLetter  = 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'
+    [String]$BigLetter    = 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z'
+    [String]$Numeral      = '1,2,3,4,5,6,7,8,9,0'
+    [String]$Symbol       = '!,",#,%,&'
+    #endregion
+
+    #Errorhandling
+    if ($Length -lt 8)
+        { Write-Error -Message 'Argument "-Length" must be greater than or equal 8.' -Category InvalidData -ErrorId 666 }
+    else
+        {
+        if ($Number -le 0)
+            { Write-Error -Message 'Argument "-Number" must be greater than 0.' -Category InvalidData -ErrorId 999 }
+        else 
+            {
+          
+            #region Generate Passwords
+            for ($x = 1; $x -le $Number ; $x++)
+                { 
+                [String]$NewPW = ""
+                $NewPW += Get-Random -InputObject $BigLetter.Split(",")
+            
+                for ($i = 1; $i -le (($Length - 2)/2) ; $i++)
+                    { 
+                    $NewPW += Get-Random -InputObject $SmallLetter.Split(",")
+                    $NewPW += Get-Random -InputObject $Symbol.Split(",")
+                    }
+            
+                $NewPW += Get-Random -InputObject $Numeral.Split(",")  
+            
+                if ($NewPW.Length -lt $Length)
+                    { $NewPW += Get-Random -InputObject $Numeral.Split(",") }
+            
+                [String[]]$Result += $NewPW
+                }
+            #endregion
+          
+            #region Password Output
+            if ($AsSecureString)
+                {
+                foreach ($Item in $Result)
+                    {
+                    $SecureResult += @([PSCustomObject]@{ Password = $Item ; SecurePassword = (ConvertTo-SecureString -String $Item -AsPlainText -Force)})  
+                    }
+                $SecureResult
+                }
+            else
+                { 
+                foreach ($Item in $Result)
+                    { $PlainResult += @([PSCustomObject]@{ Password = $Item } ) }
+                $PlainResult 
+                }
+
+            #endregion
+            }
+        }
+    }
---
+++ PW-Generator_v4_final_GUI.ps1
@@ -0,0 +1,112 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+
+$passwortgenerator = New-Object system.Windows.Forms.Form 
+$passwortgenerator.Text = "Passwortgenerator"
+$passwortgenerator.TopMost = $true
+$passwortgenerator.Width = 415
+$passwortgenerator.Height = 215
+
+$pw_count_label = New-Object system.windows.Forms.Label 
+$pw_count_label.Text = "Anzahl der Passwoerter"
+$pw_count_label.AutoSize = $true
+$pw_count_label.Width = 25
+$pw_count_label.Height = 10
+$pw_count_label.location = new-object system.drawing.point(24,32)
+$pw_count_label.Font = "Microsoft Sans Serif,10"
+$passwortgenerator.controls.Add($pw_count_label) 
+
+$pw_count_box = New-Object system.windows.Forms.TextBox 
+$pw_count_box.Text = "5"
+$pw_count_box.Width = 100
+$pw_count_box.Height = 20
+$pw_count_box.location = new-object system.drawing.point(227,32)
+$pw_count_box.Font = "Microsoft Sans Serif,10"
+$passwortgenerator.controls.Add($pw_count_box) 
+
+$pw_length_label = New-Object system.windows.Forms.Label 
+$pw_length_label.Text = "Laenge der Passwoerter"
+$pw_length_label.AutoSize = $true
+$pw_length_label.Width = 25
+$pw_length_label.Height = 10
+$pw_length_label.location = new-object system.drawing.point(24,77)
+$pw_length_label.Font = "Microsoft Sans Serif,10"
+$passwortgenerator.controls.Add($pw_length_label) 
+
+$pw_length_box = New-Object system.windows.Forms.TextBox 
+$pw_length_box.Text = "8"
+$pw_length_box.Width = 100
+$pw_length_box.Height = 20
+$pw_length_box.location = new-object system.drawing.point(228,77)
+$pw_length_box.Font = "Microsoft Sans Serif,10"
+$passwortgenerator.controls.Add($pw_length_box) 
+
+$create = New-Object system.windows.Forms.Button 
+$create.Text = "Erstellen"
+$create.Width = 70
+$create.Height = 30
+$create.location = new-object system.drawing.point(67,131)
+$create.Font = "Microsoft Sans Serif,10"
+$create.add_MouseClick({ 
+#region Variablen
+$ErrorActionPreference = "SilentlyContinue"
+$Ergebnis              = $null
+
+[String]$kleinB = 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'
+[String]$grossB = 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z'
+[String]$zahl   = '1,2,3,4,5,6,7,8,9,0'
+[String]$sonder = '!,",#,%,&'
+
+[int]$anzahl = $pw_count_box.Text
+[int]$laenge = $pw_length_box.Text
+#endregion
+
+#region Errorhandling
+if ($laenge -lt 8)
+    {
+     Write-Warning -Message "Die mindest Passwortlaenge betraegt 8 Zeichen!"
+     Write-Warning -Message "Darf aber nur aus Zahlen bestehen!"
+     Start-Sleep -Seconds 5
+     exit
+    }
+#endregion
+
+#region Passwort generieren
+for ($x = 1; $x -le $anzahl ; $x++)
+    { 
+     [String]$NewPW = ""
+     $NewPW += Get-Random -InputObject $grossB.Split(",")
+     
+     for ($i = 1; $i -le (($laenge - 2)/2) ; $i++)
+        { 
+         $NewPW += Get-Random -InputObject $kleinB.Split(",")
+         $NewPW += Get-Random -InputObject $sonder.Split(",")
+        }
+     
+     $NewPW    += Get-Random -InputObject $zahl.Split(",")  
+     
+     if ($NewPW.Length -lt $laenge)
+        { $NewPW += Get-Random -InputObject $zahl.Split(",") }
+
+     [String[]]$Ergebnis += $NewPW
+    }
+#endregion
+
+#region Passwortausgabe
+$Ergebnis.Split(";") | Out-File -FilePath $env:USERPROFILE\Desktop\Passwoerter.txt
+$passwortgenerator.Dispose()
+#endregion 
+})
+
+$passwortgenerator.controls.Add($create) 
+
+$cancel = New-Object system.windows.Forms.Button 
+$cancel.Text = "Abbrechen"
+$cancel.Width = 100
+$cancel.Height = 30
+$cancel.location = new-object system.drawing.point(277,131)
+$cancel.Font = "Microsoft Sans Serif,10"
+$cancel.add_MouseClick({$passwortgenerator.Dispose()})
+$passwortgenerator.controls.Add($cancel) 
+
+[void]$passwortgenerator.ShowDialog() 
+$passwortgenerator.Dispose() 
